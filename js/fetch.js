867f0de54139dcdbf643df95d44c51b85a855b3f
oschou <eaaosol@students.eaaa.dk>
2021-11-29 08:53:17 +0100

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -2,8 +2,26 @@ fetch("../json/data.json")
 .then(response => response.json())
 .then (data => {
     console.log(data.historie)
-    document.querySelector("#historie").innerText = data.historie
 
+    var rootElem = document.querySelector("#historie");
+
+    for (let index = 0; index < data.historie.length; index++) {
+        const element = data.historie[index];
+
+        var title = element.title;
+        var description = element.description;
+        var img = element.img;
+
+
+
+        console.log(element.title)
+
+        document.querySelector("#historie").innerText = data.historie[0].title
+        
+
+    }
+
+    //document.querySelector("#historie").innerText = data.historie[0].title
 })
 
 
