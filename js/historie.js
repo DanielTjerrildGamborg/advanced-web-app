91f6176eb65f357ddd536711b9a17cab6235b98f
oschou <eaaosol@students.eaaa.dk>
2021-11-30 12:38:21 +0100

    joe

--- js/historie.js
+++ js/historie.js
@@ -1,31 +1,42 @@
 "use strict";
 
+// =========== Product functionality =========== //
+/*
+global variables: _products _selectedProductId
+*/
+let _products = [];
+let _selectedProductId;
 
 /*
 Fetches json data from the file products.json
 */
-
-let _products = [];
 async function fetchData() {
-  const response = await fetch('js/data.json');
+  const response = await fetch('json/products.json');
   const data = await response.json();
   _products = data;
   console.log(_products);
   appendProducts(_products);
+  showLoader(false);
 }
 
 fetchData();
-
 
 function appendProducts(products) {
   let htmlTemplate = "";
   for (let product of products) {
     htmlTemplate += /*html*/`
-      <article>
-        <article>
-         <h1>${product.historie.title}
+      <article class="${product.status}">
+        <article onclick="showDetailView(${product.id})">
+          <img src="${product.img}">
+          <h2>${product.model}</h2>
+          <h3>${product.brand}</h3>
+          <p>Price: ${product.price} kr.</p>
+          <p class="status">Status: ${product.status}</p>
         </article>
-          `;
+        <button onclick="goToEdit(${product.id})">Bestil</button>
+      </article>
+    `;
   }
-  document.querySelector('#historie').innerHTML = htmlTemplate;
+  document.querySelector('#grid-container1').innerHTML = htmlTemplate;
 }
+
